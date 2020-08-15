c34fc30e8e6d8fda41a66aeef1b3fd75ac61f5ce
Ryan Quinn <rtquinn2@ncsu.edu>
2020-08-15 17:32:12 -0400

    Init Commit

---
+++ soc-btmesh-sensor-client/Self-Provision_Functions.h
@@ -0,0 +1,13 @@
+/*
+ * Self-Provision_Functions.h
+ *
+ *  Created on: Jul 15, 2020
+ *      Author: rquin
+ */
+
+#ifndef SELF_PROVISION_FUNCTIONS_H_
+#define SELF_PROVISION_FUNCTIONS_H_
+
+
+void Self_Provision_Device(void);
+#endif /* SELF_PROVISION_FUNCTIONS_H_ */
---
+++ soc-btmesh-sensor-client/TID.h
@@ -0,0 +1,20 @@
+/*
+ * TID.h
+ *
+ *  Created on: Jul 16, 2020
+ *      Author: rquin
+ */
+
+#ifndef TID_H_
+#define TID_H_
+
+#define TIMER_ID_RESTART            78
+#define TIMER_ID_FACTORY_RESET      77
+#define TIMER_ID_PROVISIONING       66
+#define TIMER_ID_SENSOR_DATA        65
+#define TIMER_ID_SENSOR_DESCRIPTOR  64
+#define TIMDER_ID_SEND_GENERIC_CLIENT_MESSAGE 80
+
+#define ONE_SECOND 32768
+
+#endif /* TID_H_ */
